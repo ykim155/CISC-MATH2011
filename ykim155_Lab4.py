a799c5a84619a5252b9ec40519cc6b6624f3ecab
ykim155 <yujin.eugene.kim@gmail.com>
2020-04-25 22:28:41 -0400

    Implementation of Pyramid and Rotation Matrix

--- ykim155_Lab4.py
+++ ykim155_Lab4.py
@@ -3,9 +3,9 @@ import math
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Number 1
 cos, sin, pi = math.cos, math.sin, math.pi
 
+# Number 1
 dt = 0.01  # time constant
 fig, ax = plt.subplots()
 l1, l2 = 4, 5
@@ -38,3 +38,33 @@ def animate(i):
 
 animination = FuncAnimation(fig, func=animate, frames=range(100), interval=1)
 plt.show()
+
+# Number 3
+
+# Create a 3D Object
+x = np.array([(0, 1, 1, 0, 0.5)
+              (0, 0, 1, 1, 0.5)
+              (0, 0, 0, 0, 0.5)])
+fig =  plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot(x[0], x[1], x[2], '.b')
+plt.show()
+
+# Create the rotation matrix to rotate about the axis
+norm = np.linalg.norm
+u = np.array([1, 0, 1])
+v = np.array([1, 0, 1])
+w = np.cross(u, w)
+
+u = u.reshape((3, 1)) / norm(u)
+v = v.reshape((3, 1)) / norm(v)
+w = w.reshape((3, 1)) / norm(w)
+
+Q = np.hstack([u, v, w])
+th = pi / 20.0
+R = cos(th) * u @ u.T - sin(th) * u @ v.T + sin(th) * v @ u.T + cos(th) * v @ v.T
+Q = w @ w.T + R
+
+# Create Animation
+
+def animate(i):
