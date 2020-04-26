c13b6310819cddcbaf1ea606a0d8897e792f2ebf
Yujin Kim <yujin.eugene.kim@gmail.com>
2020-04-25 21:18:55 -0400

    Lab 4 shear matrix and rotation matrix implementation

--- ykim155_Lab4.py
+++ ykim155_Lab4.py
@@ -1,26 +1,34 @@
 import numpy as np
 import math
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Number 1
-dt = 0.1  # time constant
-fig, ax = plt.subplot()
+cos, sin, pi = math.cos, math.sin, math.pi
+
+dt = 0.01  # time constant
+fig, ax = plt.subplots()
 l1, l2 = 4, 5
 
-x = np.array([(0, 0, 2, 2, 0), (0, 1, 1, 0, 0)])  # rectangle vertices
+x = np.array([(0, 2, 2, 0, 0), (0, 0, 1, 1, 0)])  # rectangle vertices
+
+S = np.array([(1, 0), (dt, 1)])  # shear matrix
 L = np.diag([1 + dt * l1, 1 + dt * l2])  # diagonal matrix
+R = np.array([(cos(2 * pi * dt), -sin(2 * pi * dt)), (sin(2 * pi * dt), cos(2 * pi * dt))])  # rotation matrix
 
 
 def animate(i):
     global x, L
+
     x = L @ x
+    x = S @ x
+    x = R @ x
 
     ax.clear()
 
     # draw axes
-    ax.set_xlim(-8, 10)
-    ax.set_ylim(-8, 10)
+    ax.set_xlim(-50, 100)
+    ax.set_ylim(-50, 100)
 
     ax.plot(x[0], x[1])
 
@@ -28,5 +36,5 @@ def animate(i):
     print(Vol)
 
 
-animination = FuncAnimation(fig, func=animate, frames=range(100), interval=2)
-plt.show
+animination = FuncAnimation(fig, func=animate, frames=range(100), interval=1)
+plt.show()
