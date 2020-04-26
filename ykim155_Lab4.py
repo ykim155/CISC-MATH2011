232ecf660d462ccfb6fd52bd551d310fba71fdf8
ykim155 <yujin.eugene.kim@gmail.com>
2020-04-26 00:51:36 -0400

    Continued work, plotting faces in different colors.

--- ykim155_Lab4.py
+++ ykim155_Lab4.py
@@ -17,7 +17,7 @@ L = np.diag([1 + dt * l1, 1 + dt * l2])  # diagonal matrix
 R = np.array([(cos(2 * pi * dt), -sin(2 * pi * dt)), (sin(2 * pi * dt), cos(2 * pi * dt))])  # rotation matrix
 
 
-def animate(i):
+def animate1(i):
     global x, L
 
     x = L @ x
@@ -36,18 +36,58 @@ def animate(i):
     print(Vol)
 
 
-animination = FuncAnimation(fig, func=animate, frames=range(100), interval=1)
+anim = FuncAnimation(fig, func=animate1, frames=range(100), interval=1)
 plt.show()
 
 # Number 3
 
 # Create a 3D Object
-shape = np.array([[0, 1, 1, 0, 0, 0.5, 1, 0, 0.5, 1, 0.5, 0],
-                  [0, 0, 1, 1, 0, 0.5, 0, 0, 0.5, 1, 0.5, 1],
-                  [0, 0, 0, 0, 0, 0.5, 0, 0, 0.5, 0, 0.5, 0]])
-fig = plt.figure()
+shape = np.array([[0, 0, 1, 1, 0, 0, 1, 1],
+                  [0, 1, 0, 1, 0, 1, 0, 1],
+                  [0, 0, 0, 0, 1, 1, 1, 1]])
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# ax.plot(shape[0], shape[1], shape[2],'.b')
+# plt.show()
+
+# Vertices and Faces
+i1 = [0, 2, 4, 6]
+i2 = [2, 3, 6, 7]
+i3 = [1, 3, 5, 7]
+i4 = [0, 1, 4, 5]
+B = [0, 1, 2, 3]
+T = [4, 5, 6, 7]
+
+f1 = [tuple(shape[:, i]) for i in i1]
+f2 = [tuple(shape[:, i]) for i in i2]
+f3 = [tuple(shape[:, i]) for i in i3]
+f4 = [tuple(shape[:, i]) for i in i4]
+fB = [tuple(shape[:, i]) for i in B]
+fT = [tuple(shape[:, i]) for i in T]
+
+faces = [f1, f2, f3, f4, B, T]
+colors = ['r', 'b', 'g', 'k', 'm', 'c']
+
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+fig = plt.figure()  # create new figure
 ax = fig.gca(projection='3d')
-ax.plot(shape[0], shape[1], shape[2], 'g')
+
+# plot polygonal face one at a time---- SMH
+for (f, c) in zip(faces, colors):
+    poly = Poly3DCollection([f])
+    poly.set_color(c)
+    ax.add_collection3d(poly)
+    vx = [t[0] for t in f]
+    vy = [t[1] for t in f]
+    vz = [t[2] for t in f]
+    ax.plot(vx, vy, vz, 'k', linewidth=3)
+
+ax.axis('scaled')
+ax.set_xlim(0, 2)
+ax.set_ylim(0, 2)
+ax.set_zlim(0, 2)
 plt.show()
 
 # Create the rotation matrix to rotate about the axis
@@ -65,10 +105,5 @@ th = pi / 20.0
 R = cos(th) * u @ u.T - sin(th) * u @ v.T + sin(th) * v @ u.T + cos(th) * v @ v.T
 Q = w @ w.T + R
 
-# Create Animation
 
-def animate(i):
-    global shape, Q
-    shape = R @ shape
-    ax.clear()
-    ax.plot(x[0], x[1], x[2])
+# Create Animation
